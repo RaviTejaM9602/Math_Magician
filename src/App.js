ce03093492b75f1c7c5ed95fe4cfd9cb6be21608
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-03-21 23:06:52 +0000

    Add Structure to website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [result, setResult] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <form>
+              <input type="text" value={result} />              
+          </form>
+          <div className="keypad">
+               <button>Clear</button>
+               <button>C</button>
+               <button>&divide;</button>
+               <button>7</button>
+               <button>8</button>
+               <button>9</button>
+               <button>&times;</button>
+               <button>4</button>
+               <button>5</button>
+               <button>6</button>
+               <button>&ndash;</button>
+               <button>1</button>
+               <button>2</button>
+               <button>3</button>
+               <button>+</button>
+               <button>0</button>
+               <button>.</button>
+               <button>=</button>
+          </div>
+      </div>
   );
 }
 
